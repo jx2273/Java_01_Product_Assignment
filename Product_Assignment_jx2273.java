47335550ee42ea3f309cb3fd50b63b34fb1f767a
jx2273 <jessica.xiao@utexas.edu>
2016-11-22 14:58:42 -0600

    completed assignment locally

---
+++ Product_Assignment_jx2273.java
@@ -0,0 +1,8 @@
+// Create a Java class representing a Product
+// It should be based on the template here.
+// It should be named Product_<<eid>>.java   (Product_jr007.java)
+// It should have a private sku instance variable containing your eid.
+
+class Product_Assignment_jx2273 {
+    private String eid = "jx2273";
+}
